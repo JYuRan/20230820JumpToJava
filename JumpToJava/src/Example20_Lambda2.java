40f6e0529139e8912adae83ac05dbd7a90d14407
DESKTOP-JD5JV1S\jihun <ruse216@gmail.com>
2023-09-19 12:18:26 +0900

    281p

--- JumpToJava/src/Example20_Lambda2.java
+++ JumpToJava/src/Example20_Lambda2.java
@@ -1,3 +1,9 @@
-public class Example20_Lambda2 {
+import java.util.function.BinaryOperator;
 
+public class Example20_Lambda2 {
+    public static void main(String[] args) {
+        BinaryOperator<Integer> mc = (a,b) -> a + b;
+        int result = mc.apply(3, 4);
+        System.out.println(result);
+    }
 }
---
+++ JumpToJava/src/Example22_Stream2.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class Example22_Stream2 {
+
+    public static void main(String[] args) {
+        int[] data = {5, 6 ,4, 2, 3, 1, 1, 2, 2, 4, 8};
+        int[] result = Arrays.stream(data)
+                .boxed()
+                .filter((a) -> a % 2 == 0)
+                .distinct()
+                .sorted(Comparator.reverseOrder())
+                .mapToInt(Integer::intValue)
+                .toArray()
+                ;
+    }
+}
